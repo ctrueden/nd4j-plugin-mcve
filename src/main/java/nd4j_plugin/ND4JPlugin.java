c3dadea5a7962452ddce2a6f07e4e288bedab4de
Curtis Rueden <ctrueden@wisc.edu>
2017-11-07 21:15:08 -0600

    Initial version

---
+++ src/main/java/nd4j_plugin/ND4JPlugin.java
@@ -0,0 +1,22 @@
+
+package nd4j_plugin;
+
+import org.nd4j.linalg.factory.Nd4j;
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = Command.class, menuPath = "Plugins>ND4J Plugin")
+public class ND4JPlugin implements Command {
+
+	@Parameter(type = ItemIO.OUTPUT)
+	private Object result;
+
+	@Override
+	public void run() {
+		System.out.println("Creating ND4J thing");
+		result = Nd4j.create(1, 100);
+		System.out.println("Created ND4J thing");
+	}
+}
